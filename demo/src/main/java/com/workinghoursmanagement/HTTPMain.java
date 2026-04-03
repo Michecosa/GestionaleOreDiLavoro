7560c0fe54708c2b04dd35d2b8d65f0fe273566d
Michela <micheladellagatta1@gmail.com>
2026-04-03 19:04:46 +0200

    fix: risolto conflitto porte e corretti import jakarta/javax

--- demo/src/main/java/com/workinghoursmanagement/HTTPMain.java
+++ demo/src/main/java/com/workinghoursmanagement/HTTPMain.java
@@ -1,13 +1,15 @@
 package com.workinghoursmanagement;
 
 import java.io.IOException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
 @WebServlet("/hello")
 public class HTTPMain extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
+        response.setContentType("text/plain"); // Buona pratica
         response.getWriter().println("Ciao dal Servlet!");
     }
 }
